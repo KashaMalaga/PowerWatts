5a9bd7bd189874c330c3c46b96ed89dbf9eb793f
Ramirez Ramos Jesus <jesus.ramirez1@volkswagen-groupservices.com>
2026-02-16 09:57:48 +0100

    Initial Commit

---
+++ PowerWatts/PowerWattsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PowerWattsApp.swift
+//  PowerWatts
+//
+//  Created by Ramirez Ramos, Jesus (A-GIIB-OO) on 16/2/26.
+//
+
+import SwiftUI
+
+@main
+struct PowerWattsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
